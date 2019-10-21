05b578bb93c0dc8fdd67839f85b1ee43b92e5901
Сергей Мурый ПФР <s-muryy@yandex.ru>
2019-10-21 08:33:30 +0300

    P-1 #ReorgDatabase

--- ReorgWeeks/ReorgWeeks/MainReorgWeeks.cs
+++ ReorgWeeks/ReorgWeeks/MainReorgWeeks.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
 
 namespace ReorgWeeks
 {
@@ -10,6 +15,46 @@ namespace ReorgWeeks
     {
         static void Main(string[] args)
         {
+            DateTime today = DateTime.Now;
+            StartReorg obj = new StartReorg();
+            INIManager ini = new INIManager(Path.Combine(Environment.CurrentDirectory, "config.ini"));
+            String var;
+
+            obj.ControlcenterDB2Close();
+
+            var = ini.GetPrivateString("checkReorg", "value");
+          
+
+            foreach (String date in StartReorg.dateReorg())
+            {
+                if(today.ToString("dd.MM.yyyy") == date)
+                {
+                    if (Int32.Parse(var) % 2 != 0)
+                    {
+                        //obj.StartBatFile(@"/testreorg & start_reorg_week1.bat");
+                        obj.StartBatFile("start_reorg_week1.bat");
+                        //Меняем значение в App.config на четное число
+                        ini.WritePrivateString("checkReorg", "value", "2");
+                        //Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                        //currentConfig.AppSettings.Settings["checkReorg"].Value = "2";
+                        //currentConfig.Save(ConfigurationSaveMode.Full);
+                        //ConfigurationManager.RefreshSection("appSettings");
+                    }
+                    if (Int32.Parse(var) % 2 == 0)
+                    {
+                        obj.StartBatFile("start_reorg_week2.bat");
+                        //Меняем значение в App.config на нечетное число
+                        ini.WritePrivateString("checkReorg", "value", "1");
+                        //Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                        //currentConfig.AppSettings.Settings["checkReorg"].Value = "1";
+                        //currentConfig.Save(ConfigurationSaveMode.Modified);
+                        //ConfigurationManager.RefreshSection("appSettings");
+                    }
+                }
+            }
+
+            Console.WriteLine("Регламентные работы завершины. Посмотрите log выполнения регламентных работ.");
+            Console.ReadKey();
         }
     }
 }
---
+++ ReorgWeeks/ReorgWeeks/StartReorg.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ReorgWeeks
+{
+    public class StartReorg
+    {
+        //Находим все субботы в текущем месяце
+        public static List<String> dateReorg()
+        {
+            DateTime now = DateTime.Now;
+
+            int year = now.Year;
+            int month = now.Month;
+            int dayMonth = DateTime.DaysInMonth(year, month);
+            String dayWeek;
+            List<String> col = new List<String>();
+
+            for(int i = 1; i<=dayMonth; i++)
+            {
+                DateTime date = new DateTime(year, month, i);
+                if(date.ToString("dddd") == "понедельник")
+                {
+                    dayWeek = date.ToString("dd.MM.yyyy");
+                    col.Add(dayWeek);
+                }
+            }
+
+            return col;
+        }
+
+        //Закрываем Центер управления DB2
+        public void ControlcenterDB2Close()
+        {
+            Process[] ps1 = System.Diagnostics.Process.GetProcessesByName("javaw"); //Имя процесса
+            foreach (Process p1 in ps1)
+            {
+                p1.Kill();
+            }
+        }
+
+        //Запускаем батник для реорганизации
+        public void StartBatFile(String path)
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = "cmd.exe";
+                proc.StartInfo.Arguments = @"/C cd C:\testreorg\ & " + path;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+                int exitCode = proc.ExitCode;
+                proc.Close();
+              }
+             catch(Exception)
+              {
+                 Console.WriteLine("Bat файл по заданному пути не найден.");
+                  
+             }
+            
+        }
+
+    }
+}
